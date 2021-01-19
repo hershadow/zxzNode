990352b3f931cc9facb36fbd9759c7f88e3c227b
hershadow <hershadow@users.noreply.github.com>
2021-01-19 20:57:09 +0800

    windows_update1
    试一试

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
-var http = require('http');
+//const cheerio = require("cheerio");
+const axios = require('axios')
+
+let httpUrl = "https://bbs-api.mihoyo.com/post/wapi/getPostReplies?gids=2&is_hot=true&last_id=20&post_id=1958320&size=20"
+axios.get(httpUrl).then((res)=>{
+        console.log(res)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * var http = require('http');
 http.createServer(function(req,res){
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Hello World!www.linuxidc.com');
 }).listen(8080);
 console.log('Server started on localhost:8080; press Ctrl-C to terminate...!');
+ */
